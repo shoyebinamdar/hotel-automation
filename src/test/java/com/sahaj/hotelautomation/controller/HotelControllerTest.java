e6149b6dbc318d99756a18905429ab457b545d68
shoyebinamdar <shoyeb.inamdar@volkswagen.co.in>
2019-09-29 20:51:26 +0530

    Refactored controller tests. Mostly done with changes

--- src/test/java/com/sahaj/hotelautomation/controller/HotelControllerTest.java
+++ src/test/java/com/sahaj/hotelautomation/controller/HotelControllerTest.java
@@ -8,6 +8,7 @@ import com.sahaj.hotelautomation.entities.sensors.MotionSensor;
 import com.sahaj.hotelautomation.equipments.ElectronicEquipment;
 import com.sahaj.hotelautomation.services.MovementService;
 import com.sahaj.hotelautomation.utils.EquipmentType;
+import com.sahaj.hotelautomation.utils.HotelFactory;
 import com.sahaj.hotelautomation.utils.State;
 import org.junit.Test;
 
@@ -22,112 +23,45 @@ public class HotelControllerTest {
 
     @Test
     public void shouldGiveValidConsumtionMovementInTwoCorridors() throws Exception {
-        MainCorridor mainCorridor = MainCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.ON, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10))
-                ).build();
-
-        SubCorridor subCorridor1 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                )).build();
-
-        SubCorridor subCorridor2 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                )).build();
-
-        SubCorridor subCorridor3 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                ))
-                .build();
-
-        SubCorridor subCorridor4 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                )).build();
-
-
-        Floor floor1 = Floor.builder()
-                .mainCorridors(Collections.singletonList(mainCorridor))
-                .subCorridors(Arrays.asList(subCorridor1, subCorridor2))
-                .build();
-        Floor floor2 = Floor.builder()
-                .mainCorridors(Collections.singletonList(mainCorridor))
-                .subCorridors(Arrays.asList(subCorridor3, subCorridor4))
-                .build();
-
-        Hotel hotel = Hotel.builder()
-                .floors(Arrays.asList(floor1, floor2))
-                .build();
+        Hotel hotel = HotelFactory.create(2, 1, 2);
 
         HotelController hotelController = new HotelController(hotel);
 
-        floor1.registerCorridors();
-        sensor.registerCorridor(subCorridor1);
+        hotel.getFloors().get(0).registerCorridors();
+        sensor.registerCorridor(hotel.getFloors().get(0).getSubCorridors().get(0));
 
         movementService.triggerMovement(sensor);
 
         assertEquals(65, hotelController.consumption());
-        assertEquals(State.ON, subCorridor1.getEquipments().get(0).getState());
-        assertEquals(State.ON, subCorridor1.getEquipments().get(1).getState());
-        assertEquals(State.OFF, subCorridor2.getEquipments().get(0).getState());
-        assertEquals(State.OFF, subCorridor2.getEquipments().get(1).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(0).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(1).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(0).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(1).getState());
     }
 
     @Test
     public void shouldGiveValidConsumptionWhenMovementFollowedByNoMovement() throws Exception {
-        MainCorridor mainCorridor = MainCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.ON, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10))
-                ).build();
-
-        SubCorridor subCorridor1 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                )).build();
-        SubCorridor subCorridor2 = SubCorridor.builder()
-                .equipments(Arrays.asList(
-                        new ElectronicEquipment(EquipmentType.LIGHT, State.OFF, 5),
-                        new ElectronicEquipment(EquipmentType.AC, State.ON, 10)
-                )).build();
-
-        Floor floor1 = Floor.builder()
-                .mainCorridors(Collections.singletonList(mainCorridor))
-                .subCorridors(Arrays.asList(subCorridor1, subCorridor2))
-                .build();
-
-        Hotel hotel = Hotel.builder()
-                .floors(Arrays.asList(floor1))
-                .build();
+        Hotel hotel = HotelFactory.create(1, 1, 2);
 
         HotelController hotelController = new HotelController(hotel);
 
-        floor1.registerCorridors();
-        sensor.registerCorridor(subCorridor1);
+        hotel.getFloors().get(0).registerCorridors();
+        sensor.registerCorridor(hotel.getFloors().get(0).getSubCorridors().get(0));
 
         movementService.triggerMovement(sensor);
 
         assertEquals(30, hotelController.consumption());
-        assertEquals(State.ON, subCorridor1.getEquipments().get(0).getState());
-        assertEquals(State.ON, subCorridor1.getEquipments().get(1).getState());
-        assertEquals(State.OFF, subCorridor2.getEquipments().get(0).getState());
-        assertEquals(State.OFF, subCorridor2.getEquipments().get(1).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(0).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(1).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(0).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(1).getState());
 
         movementService.triggerStagnation(sensor);
 
         assertEquals(35, hotelController.consumption());
-        assertEquals(State.OFF, subCorridor1.getEquipments().get(0).getState());
-        assertEquals(State.ON, subCorridor1.getEquipments().get(1).getState());
-        assertEquals(State.OFF, subCorridor2.getEquipments().get(0).getState());
-        assertEquals(State.ON, subCorridor2.getEquipments().get(1).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(0).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(0).getEquipments().get(1).getState());
+        assertEquals(State.OFF, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(0).getState());
+        assertEquals(State.ON, hotel.getFloors().get(0).getSubCorridors().get(1).getEquipments().get(1).getState());
     }
 }
